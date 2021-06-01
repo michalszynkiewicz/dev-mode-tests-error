536d0f080ce037d08aad8db04461787a095042fa
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-06-01 13:10:55 +0200

    with failing assertion example

--- src/test/java/com/example/FooTest.java
+++ src/test/java/com/example/FooTest.java
@@ -1,27 +1,18 @@
 package com.example;
 
-import io.quarkus.grpc.GrpcClient;
 import io.quarkus.test.junit.QuarkusTest;
-import io.smallrye.mutiny.Uni;
 import org.junit.jupiter.api.Test;
-
-import java.time.Duration;
 
 @QuarkusTest
 public class FooTest {
-    @GrpcClient
-    Foo foo;
-
 
     @Test
-    void shouldFoo() {
-        Uni<MyNew.Dto> ping = foo.ping(MyNew.Dto.newBuilder().build());
-        ping.await().atMost(Duration.ofSeconds(5));
+    void shouldFoo2() throws InterruptedException {
+        int i = 0;
+        int j = i+1;
+        for (int k = 0; k < 2; k++) {
+            j++;
+        }
+        assert j == i;
     }
-//    @Test
-//    void shouldFoo2() {
-//        Uni<MyNew.Dto> ping = foo.ping(MyNew.Dto.newBuilder().build());
-//        ping.await().atMost(Duration.ofSeconds(5));
-//        assert 1 == 0;
-//    }
 }
---
+++ src/test/java/com/example/PlainTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class PlainTest {
+    @Test
+    void foo() {
+        assert 1 == 0;
+    }
+}
